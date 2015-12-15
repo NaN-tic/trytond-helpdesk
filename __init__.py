6cf4dca9887385011ccf597956937cb87c67bf09
resteve <resteve@zikzakmedia.com>
2015-12-15 16:12:35 +0100

    Getmail kind and server parameter

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@
 from trytond.pool import Pool
 from .configuration import *
 from .helpdesk import *
+from .getmail import *
 
 def register():
     Pool.register(
@@ -12,4 +13,5 @@ def register():
         HelpdeskTalk,
         HelpdeskLog,
         HelpdeskAttachment,
+        GetmailServer,
         module='helpdesk', type_='model')
